7c443fa3b4a34dd997b263b8c9e6ee0a6d43ddae
Carlos Diaz <carlos12one@hotmail.com>
2023-06-26 13:54:50 -0500

    asds

--- src/Data/sources/remote/api/ApiBackend.tsx
+++ src/Data/sources/remote/api/ApiBackend.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiBackend = axios.create({
-  baseURL: "http://192.168.1.13:7000/api",
+  baseURL: "https://backend-movie-y8xq.onrender.com/api",
   headers: {
     "Content-type": "application/json",
   },
